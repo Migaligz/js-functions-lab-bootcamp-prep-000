dc4d5fa0c51419690f47f83cf2673cb4efe8d739
Aaron Isberg <aaronisberg@gmail.com>
2018-08-26 20:42:52 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,5 +4,7 @@ function happyHolidays(string) {
 function happyHolidayTo(holiday, name) {
   return `Happy ${holiday}, ${name}!`
  
-
 }
+function happyHolidaysTo(name)
+  return `Happy holidays, ${name}!`
+  }
